9a9801865e4dde1819cda6823754e42b478ef796
zhen <mail@onezen.cc>
2017-04-13 10:22:30 +0800

    pdf demo

---
+++ iOS/pdfDemo/pdfDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pdfDemo
+//
+//  Created by wangzhen on 17/4/13.
+//  Copyright © 2017年 onezen.cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
