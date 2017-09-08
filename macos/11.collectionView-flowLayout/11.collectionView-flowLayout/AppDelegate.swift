bb5b2a491d4b1eecd14646595cce0bb2b26e3e23
wz <leafnet02@qq.com>
2017-09-08 17:11:52 +0800

    collectionView layout

---
+++ macos/11.collectionView-flowLayout/11.collectionView-flowLayout/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  11.collectionView-flowLayout
+//
+//  Created by wz on 2017/9/8.
+//  Copyright © 2017年 cc.onezen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos/12.collectionView-gridLayout/11.collectionView-flowLayout/ImageItem.swift
@@ -0,0 +1,37 @@
+//
+//  ImageItem.swift
+//  11.collectionView-flowLayout
+//
+//  Created by wz on 2017/9/8.
+//  Copyright © 2017年 cc.onezen. All rights reserved.
+//
+
+import Cocoa
+
+class ImageItem: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        print("viewDidLoad")
+        view.wantsLayer = true
+        view.layer?.backgroundColor = NSColor.cyan.cgColor
+    }
+    
+    override var isSelected: Bool {
+        didSet{
+            super.isSelected = isSelected
+            renderSelectedColor(isSelect: isSelected)
+        }
+    }
+    
+    func renderSelectedColor(isSelect: Bool){
+        if isSelected {
+            view.layer?.backgroundColor = NSColor.lightGray.cgColor
+        }else{
+            view.layer?.backgroundColor = NSColor.cyan.cgColor
+        }
+    }
+    
+    
+}
