386ebcd35d483412bdd96cf74a89e43a3438426d
wz <leafnet02@qq.com>
2017-09-16 17:16:37 +0800

    cocopods & ui

--- macos/18.CocoaSwiftPlayer/18.CocoaSwiftPlayer/AppDelegate.swift
+++ macos/18.CocoaSwiftPlayer/18.CocoaSwiftPlayer/AppDelegate.swift
@@ -7,14 +7,15 @@
 //
 
 import Cocoa
+import RealmSwift
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ macos/18.CocoaSwiftPlayer/Pods/Target Support Files/Pods-18.CocoaSwiftPlayer/Pods-18.CocoaSwiftPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_18_CocoaSwiftPlayer : NSObject
+@end
+@implementation PodsDummy_Pods_18_CocoaSwiftPlayer
+@end
