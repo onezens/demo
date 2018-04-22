7f2a33839802adeea1c67e1db394be4c1ecd100d
onezens <mail@onezen.cc>
2018-04-22 16:24:36 +0800

    cpp

---
+++ cppDemos/cppDemos/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  cppDemos
+//
+//  Created by wz on 2018/4/21.
+//  Copyright © 2018年 wz. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    //print
+    cout << "input random string: ";
+    
+    string a;
+    //input
+    cin >> a;
+    cout << "input: " << a << endl;
+    
+    //run shell
+    //system("pause");
+    return 0;
+}
