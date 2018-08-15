43b5e8511af62ea4b4bae07b7ff2de9c0b09309d
wz <mail@onezen.cc>
2018-08-15 15:19:19 +0800

    add cycript demo for ios project

---
+++ CycriptDemo/CycriptDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CycriptDemo
+//
+//  Created by wz on 2018/8/15.
+//  Copyright © 2018 wz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CycriptDemo/CycriptDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CycriptDemo
+//
+//  Created by wz on 2018/8/15.
+//  Copyright © 2018 wz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
